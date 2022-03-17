cd0cb3cc886949d0a707866f7466ca10e9e28bdf
Guilherme Marques <guilherme.marques13@outlook.com>
2022-03-16 22:22:11 -0300

    Alteracao codigo fonte do projeto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {Routes } from '@angular/router'
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
+
+
+export const rootRouterConfig: Routes = [
+    {path: '' , redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'contato', component: ContatoComponent},
+    {path: 'sobre', component: SobreComponent},
+    {path: 'produtos', component: ListaProdutoComponent},
+    {path: 'produtos-detalhe/:id', component: ListaProdutoComponent},
+
+];
